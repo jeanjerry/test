69675a70c8a49d2bab81c10ccd4f62fcf8c6b81f
jeanjerry <C109154229@nkust.edu.tw>
2023-11-08 21:56:11 +0800

    中轉英

---
+++ lib/vision_detector_views/label_detector_view/face_class.dart
@@ -0,0 +1,18 @@
+export 'detect_smile.dart';
+export 'detect_flick_tongue.dart';
+export 'detect_open_mouth.dart';
+export 'detect_pout.dart';
+export 'detect_pursed_lips.dart';
+export 'detect_tongue.dart';
+export 'detect_chin_movement.dart';
+export 'detect_headneck_bend.dart';
+/*
+   tongue吐舌
+   pursed_lips抿唇
+   open_mouth張嘴說阿
+   smile微笑
+   pout嘟嘴
+   flick_tongue彈舌
+   chin_movement下巴運動
+   headneck_bend頭頸運動
+*/
